87300bef0fe51a941b32f40dae582e912196fce5
abhannan1 <122967255+abhannan1@users.noreply.github.com>
2024-04-16 22:15:20 +0500

    Update dbConfig.js

--- connection/dbConfig.js
+++ connection/dbConfig.js
@@ -4,7 +4,7 @@ const { DB_URL } = require("../config")
 const connection = async()=>{
     try{
         mongoose.set('strictQuery', false)
-        const conn = await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        const conn = await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: truex,  dbName:"Crypto"  })
         console.log(`Connected Successfully to ${conn.connection.host}`)
 
     } catch(err){
